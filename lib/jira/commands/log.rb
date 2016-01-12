0173ce771876c98357236478c45425301ffebe33
sdarayan <sdarayan@meraki.com>
2016-01-11 17:06:34 -0800

    log.rb: Add "logu" command to update worklog entries
    Users should be able to update a worklog entry since
    they may make mistakes when adding them. To update
    worklog entries users issue "logu" command, enter
    the index of the entry to update, and re-enter
    the time spent.

--- lib/jira/commands/log.rb
+++ lib/jira/commands/log.rb
@@ -9,7 +9,7 @@ module Jira
       end
     end
 
-    desc "logs", "Lists the worklog of the input ticket"
+    desc "logs", "Lists work against the input ticket"
     def logs(ticket=Jira::Core.ticket)
       self.api.get("issue/#{ticket}/worklog") do |json|
         worklogs = json['worklogs']
@@ -30,5 +30,55 @@ module Jira
       end
     end
 
+    desc "logu", "Updates work against the input ticket"
+    def logu(ticket=Jira::Core.ticket)
+      if self.io.agree("List worklogs for ticket #{ticket}")
+        logs(ticket)
+      end
+      idx = self.get_worklog_idx("update")
+      if idx < 0
+        puts "No worklog updated."
+        return
+      end
+      time_spent = self.io.ask("Time spent on #{ticket}").strip
+      if time_spent.empty?
+        puts "No worklog updated."
+        return
+      end
+      self.api.get("issue/#{ticket}/worklog") do |json|
+        worklogs = json['worklogs']
+        if idx < worklogs.count
+          id = worklogs[idx]['id']
+          self.api.put("issue/#{ticket}/worklog/#{id}", { timeSpent: time_spent }) do |json|
+            puts "Successfully updated #{time_spent} on #{ticket}."
+            return
+          end
+        end
+      end
+      puts "No worklog updated."
+    end
+
+    protected
+
+      #
+      # Prompts the user for a worklog index, then returns the
+      # worklog index; failure is < 0
+      #
+      # @param description [String] describes the user prompt
+      #
+      # @return idx [Integer] asked comment index
+      #
+      def get_worklog_idx(description = "")
+        idx = self.io.ask("Index for worklog to #{description}").strip
+        if !idx.empty?
+          idx = idx.to_i
+          if idx < 0
+            idx = -1
+          end
+        else
+          idx = -1
+        end
+        idx
+      end
   end
 end
