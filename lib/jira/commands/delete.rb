e95aec80df186707f3369ed36647ce02e72c3ac3
sdarayan <sdarayan@meraki.com>
2016-01-11 23:03:14 -0800

    delete.rb: No-op, change Jira to JIRA

--- lib/jira/commands/delete.rb
+++ lib/jira/commands/delete.rb
@@ -1,7 +1,7 @@
 module Jira
   class CLI < Thor
 
-    desc "delete", "Deletes a ticket in Jira and the git branch"
+    desc "delete", "Deletes a ticket in JIRA and the git branch"
     option :force, :type => :boolean, :default => false
     def delete(ticket=Jira::Core.ticket)
       force = options[:force]
